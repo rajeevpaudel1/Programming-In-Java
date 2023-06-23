a930d89c42cd71f2a9097ea7d0814f19dd2eba52
Rajeev Paudel <rajeev11paudel@gmail.com>
2023-06-23 16:00:37 +0545

    New login form with event handling

---
+++ Swings/LoginForm/tempCodeRunnerFile.java
@@ -0,0 +1,72 @@
+//A login form is created using 2 labels, 1 textfield, 1 passwordfield and 1 button
+//We have used the event handling in the following form
+//the program is written in such a way that it will display the username and password in the terminal
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class MyFrame extends JFrame implements ActionListener{
+
+    Container c;
+    JLabel label1, label2;
+    JTextField user;
+    JPasswordField psw;
+    JButton btn;
+
+    MyFrame(){
+       
+        setBounds(100,100, 400,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Login Form");
+
+        c=getContentPane();
+        c.setLayout(null);
+
+        label1= new JLabel("Username");
+        label2= new JLabel("Password");
+
+        label1.setBounds(10, 50, 100,20);
+        label2.setBounds(10,100,100,20);
+
+        c.add(label1);
+        c.add(label2);
+
+        user=new JTextField();
+        user.setBounds(120,50,120,20);
+        c.add(user);
+
+        psw=new JPasswordField();
+        psw.setBounds(120, 100, 120, 20);
+        c.add(psw);
+
+
+        btn= new JButton("Submit");
+        btn.setBounds(100,150,100,20);
+        c.add(btn);
+
+
+
+         setVisible(true);
+         btn.addActionListener(this);
+         setVisible(true);
+
+    }
+    public void actionPerformed(ActionEvent e){
+        System.out.println("Username: "+user.getText());
+        System.out.println("Password: "+psw.getText());
+
+    }
+}
+
+
+
+class LoginForm{
+
+    public static void main(String[] args) {
+        MyFrame frame =new MyFrame();
+
+        
+    }
+}
--- Swings/LoginForm1/LoginForm.java
+++ Swings/LoginForm1/LoginForm.java
@@ -1,9 +1,66 @@
 //we'll design the same form using the anonymous inner class method because i feels it was the easy method
-
+// we have used event handling here 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class LoginForm {
+    public static void main(String[] args) {
+        JFrame frame= new JFrame();
+        frame.setBounds(100,100,500,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Login Form");
+        Container c= frame.getContentPane();
+        c.setLayout(null);
+
+        //for the label we can write like this
+        JLabel label1 = new JLabel("Name: ");
+        JLabel label2 = new JLabel("Semester: ");
+        JLabel label3 = new JLabel("Roll No: ");
+        JLabel label4 = new JLabel("College: ");
+
+        label1.setBounds(50,50,100,30);
+        label2.setBounds(50,100,100,30);
+        label3.setBounds(50,150,100,30);
+        label4.setBounds(50,200,100,30);
+
+        c.add(label1);
+        c.add(label2);
+        c.add(label3);
+        c.add(label4);
+
+        //Now letus add 4 textfield
+        JTextField t1= new JTextField();
+        JTextField t2= new JTextField();
+        JTextField t3= new JTextField();
+        JTextField t4= new JTextField();
+
+        t1.setBounds(150, 50, 150, 40);
+        t2.setBounds(150, 100, 150, 40);
+        t3.setBounds(150, 150, 150, 40);
+        t4.setBounds(150, 200, 150, 40);
+
+        c.add(t1);
+        c.add(t2);
+        c.add(t3);
+        c.add(t4);
+
+        //Let us add the button here
+        JButton btn =new JButton("Submit");
+        btn.setBounds(100, 300, 100, 30);
+        c.add(btn);
+        frame.setVisible(true);
+
+        // here we'll add the event handling that will stole your data 
+        // HAHA!!
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                JLabel lab= new JLabel("Hurray !!! I stole your data");
+                lab.setBounds(100, 350, 300, 30);
+                c.add(lab);
+            }
+        });
+    }
     
+
 }
